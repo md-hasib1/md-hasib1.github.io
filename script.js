ee63d40508dd3c6df385f4e59e800e9a08f9c0fd
[mdhasibhasan <[hffhhasib@gmail.com]>
2024-09-26 14:05:38 +0600

    first Committ

--- script.js
+++ script.js
@@ -1,84 +1,54 @@
-// tyeping script Js -->
-var typed = new Tyepd(".typing" ,{
-strings:["Wev Developer" ,"Web Designer" ,"Youtuber"],
-tyepSpeed:80,
-tyepBack:80,
+// Typing Script JS
+var typed = new Typed(".typing",{
+    strings: ["Web Designer", "Web Developer", "Youtuber","Programmer"],
+    typeSpeed: 80,
+    backSpeed: 80,
 });
 
-var typed = new Tyepd(".tyeping-2" ,{
-    strings:["Wev Developer" ,"Web Designer" ,"Youtuber",],
-    tyepSpeed:80,
-    tyepBack:80,
-    });
+var typed = new Typed(".typing-2",{
+    strings: ["Web Developer", "Web Designer", "Youtuber" , "Programmer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+});
 
+// Show/Hide FAQs answer
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
 
-
-      /*====>>>>>>.  show  FAQ ans  ======> */
-      const faqs = document.querySelectorAll('.faq')
-
-      faqs.forEach(faq =>{
-        faq.addEventListener('click', () =>{
-            faq.classList.toggle('open')
-
-
-    
-        // change icon
-
-        const icon =faq.querySelector('.faq_icon');
-        if(icon.className === 'bx bx-plus'){
-            icon.className = 'bx bx-minus'
+        //change icon
+        const icon = faq.querySelector('.faq_icon i');
+        if (icon.className === 'fa-solid fa-plus'){
+            icon.className = 'fa-solid fa-minus';
         }
         else{
-            icon.className = 'bx bx-plus'
+            icon.className = 'fa-solid fa-plus';
         }
-        })
-     })
 
+    })
+})
 
-
-
-    // Show/Hide FAQs answer
-// const faqs = document.querySelectorAll('.faq');
-// faqs.forEach(faq => {
-//     faq.addEventListener('click', () => {
-//         faq.classList.toggle('open');
-
-
-
-
-//     })
-// })
-
-
-const menu =document.querySelector('.nav_menu');
+//Show/hide nav menu
+const menu = document.querySelector('.nav_menu');
 const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
 
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
 
-const closeBtn = document.querySelector('#close-menu-btn')
-
-
-
-menuBtn.addEventListener('click',function() {
-  menu.style.display = "flex"
-  closeBtn.style.display = "inline - block"
-    menuBtn.style.display = "none"
+//close nav menu
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
 }
+closeBtn.addEventListener('click', closeNav);
 
-)
-
-const closeNav = () =>{
-    menu.style.display ="none"
-    closeBtn.style.display = "none"
-    menuBtn.style.display ="inline-block"
-}
-
-closeBtn.addEventListener('click ',closeNav)
-
-
-
-
-
-window.addEventListener('scroll' ,() =>{
-    document.querySelector('nav').classList.toggle("window-scroll",window.scrollY>100)
-}
-)
+//nav color change on scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY>100);
+})
